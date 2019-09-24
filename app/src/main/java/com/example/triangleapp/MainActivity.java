2e60921b4dae515e81e12c2b9157a40397b9321c
kevvvnguyen <kevinknguyen24@gmail.com>
2019-09-24 13:04:22 -0700

    all files added

--- app/src/main/java/com/example/triangleapp/MainActivity.java
+++ app/src/main/java/com/example/triangleapp/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
+
+
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText userValues;
     private TextView resultsText;
     private Button calculateBtn;
+    private Button exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         calculateBtn = (Button) findViewById(R.id.calculateBtn);
         userValues = (EditText) findViewById(R.id.userValues);
         resultsText = (TextView) findViewById(R.id.resultsText);
+        exitBtn = (Button) findViewById(R.id.exitButton);
 
         // on click listeners...
         calculateBtn.setOnClickListener(this);
+        exitBtn.setOnClickListener(this);
 
     }
 
@@ -45,10 +52,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Valid input; get results and display to screen
             resultsText.setText(new Triangle(floatArr).getTriangleType());
         }
+
+        //exit button
+        if (view == exitBtn) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Exit");
+            builder.setMessage("Are you sure you want to exit?");
+            builder.setPositiveButton("Exit now", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    finish();
+
+                }
+            });
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    dialogInterface.dismiss();
+                }
+            });
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
+
         // add other buttons here if needed...
+
     }
 
-    public float[] parseUserInput() {
+     public float[] parseUserInput() {
 
         String userInput = userValues.getText().toString();
         String[] stringArr;
